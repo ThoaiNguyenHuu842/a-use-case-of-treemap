dfe9ef0d318c1dfa5fa8443498704a5828d0952f
LAPTOP-0OHSO3Q8\thoai <thoainguyen842@gmail.com>
2023-06-04 12:00:58 +0700

    update README

--- README.md
+++ README.md
@@ -3,9 +3,26 @@
 Demo a real use case of TreeMap in a fintech platform which uses TreeMap to store daily pricing of securities (or stocks). 
 The pricing data of a date can be null (due to no transaction at this date), at this case, the data of the nearest date should be used.
 
+Let's dig into an example with the daily pricing of stock **"NT2"** from **01-Jan-2023 to 15-Jan-2023** as below.
+
+| Date        | Price           |
+| ------------- |:-------------:| 
+| 01/01/2023      | 100.5 | 
+| 02/01/2023      | 100.6      |
+| 03/01/2023 | 100.7      |
+| 07/01/2023 | 99      |
+| 09/01/2023 | 102      |
+| 15/01/2023 | 87.4      |
+
+
+Within this example, if we bought 50 units of this stock on 07/01/2023, the value of this transaction is _****99x50=4,950****_. 
+
+However, if we bought 30 units on 11/01/2023, there is no price data on 11/01/2023 so the price data on 09/01/2023 is selected (the nearest day of 11/01/2023). 
+Hence, the value of this transaction is _**102x30=3,060**_.
 
 ## Tech stack
 
 JDK 11, Gradle
 
 ## How to get started?
+After building the project, we can run the **DailyPricingServiceTest** to see how to this example works.
--- src/test/java/DailyPricingServiceTest.java
+++ src/test/java/DailyPricingServiceTest.java
@@ -11,12 +11,15 @@ public class DailyPricingServiceTest {
     DailyPricingService dailyPricingService = new DailyPricingService();
     DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     dailyPricingService.add("NT2", dateFormat.parse("01/01/2023"), BigDecimal.valueOf(100.5));
-    dailyPricingService.add("NT2", dateFormat.parse("01/01/2023"), BigDecimal.valueOf(100.6));
+    dailyPricingService.add("NT2", dateFormat.parse("02/01/2023"), BigDecimal.valueOf(100.6));
     dailyPricingService.add("NT2", dateFormat.parse("03/01/2023"), BigDecimal.valueOf(100.7));
     dailyPricingService.add("NT2", dateFormat.parse("07/01/2023"), BigDecimal.valueOf(99));
     dailyPricingService.add("NT2", dateFormat.parse("09/01/2023"), BigDecimal.valueOf(102));
     dailyPricingService.add("NT2", dateFormat.parse("15/01/2023"), BigDecimal.valueOf(87.4));
 
-    Assertions.assertEquals(dailyPricingService.getPrice("NT2", dateFormat.parse("11/01/2023")), BigDecimal.valueOf(102), "No pricing data at 11/01/2023, so the date at 09/01/2023 should be used");
+    Assertions.assertEquals(dailyPricingService.getPrice("NT2", dateFormat.parse("11/01/2023")), BigDecimal.valueOf(102),
+      "No price data on 11/01/2023, so the date on 09/01/2023 must be selected");
+    Assertions.assertEquals(dailyPricingService.getPrice("NT2", dateFormat.parse("07/01/2023")), BigDecimal.valueOf(99),
+      "The price data on 07/01/2023 must be 99");
   }
 }
